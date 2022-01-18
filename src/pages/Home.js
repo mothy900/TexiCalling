f8e4e57844d826c382c0fa40e5b8007c9e3d4bf1
mothy900 <mothy900@naver.com>
2022-01-18 18:38:07 +0900

    home location

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import useWatchLocation from "../hooks/userCurrentPosition";
 
 function Home() {
-  return <div>Home page</div>;
+  let lat, long;
+  if (navigator.geolocation) {
+    // GPS를 지원하면
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+        console.log(lat, long);
+      },
+      function (error) {
+        console.error(error);
+      },
+      {
+        enableHighAccuracy: false,
+        maximumAge: 0,
+        timeout: Infinity,
+      }
+    );
+  } else {
+    alert("GPS를 지원하지 않습니다");
+    return;
+  }
+
+  return (
+    <div>
+      <header>
+        <div>
+          <input placeholder="반려동물을 등록해 보세요."></input>
+        </div>
+        <div>
+          <h3>오늘은 멍이요와 어디로 가실건가요.</h3>
+          <div>
+            <div className="callTaxi">
+              <a href="/Calltaxi">
+                <img src={require("../img/taxi.jpg")}></img>
+              </a>
+            </div>
+            <div className="checkBook">
+              <a href="/CheckBook">
+                <img src={require("../img/calender.jpg")}></img>
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 export default Home;
